c96413e15997d4b9f80ae00a6b676fdae9d2c8a3
Gennady Badinov <gennady.badinov@gmail.com>
2024-04-06 22:45:54 +0300

    test: test new feature

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Gennady")
-	want := "Hello, Gennady"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Gennady")
+		want := "Hello, Gennady"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, World' if an emptry string is provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
