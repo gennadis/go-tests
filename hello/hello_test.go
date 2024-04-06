3f81d95f8ab8d468a2cd949adff66bc0ba399e50
Gennady Badinov <gennady.badinov@gmail.com>
2024-04-06 22:35:46 +0300

    test: cover name in a greetings

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Gennady")
+	want := "Hello, Gennady"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
