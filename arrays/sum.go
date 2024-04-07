3abbaaf60bb6794b2926636784e2a712d7f0e97a
Gennady Badinov <gennady.badinov@gmail.com>
2024-04-07 17:33:51 +0300

    refactor: use append slice method

--- arrays/sum.go
+++ arrays/sum.go
@@ -9,10 +9,9 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lenOfNumbers := len(numbersToSum)
-	sums := make([]int, lenOfNumbers)
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 	return sums
 }
