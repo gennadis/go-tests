8fb6eee06e3f957bc7d19e53afaf7a02adb78228
Gennady Badinov <gennady.badinov@gmail.com>
2024-04-08 12:42:10 +0300

    test: refactor tests

--- pointers/walltet_test.go
+++ pointers/walltet_test.go
@@ -3,16 +3,19 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
-		wallet.Deposit(10)
 
+	assertBalance := func(t testing.TB, wallet Wallet, want Tugbacoin) {
+		t.Helper()
 		got := wallet.Balance()
-		want := Tugbacoin(10)
-
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(10)
+		assertBalance(t, wallet, Tugbacoin(10))
 
 	})
 	t.Run("withdraw", func(t *testing.T) {
@@ -20,12 +23,7 @@ func TestWallet(t *testing.T) {
 		wallet.Deposit(10)
 		wallet.Withdraw(5)
 
-		got := wallet.Balance()
-		want := Tugbacoin(5)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Tugbacoin(5))
 
 	})
 
