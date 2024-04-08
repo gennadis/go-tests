1ba4fd3c5ee718d627163a8c002e0eb2ae86b9a0
Gennady Badinov <gennady.badinov@gmail.com>
2024-04-08 12:34:44 +0300

    feat: use pointers

---
+++ pointers/wallet.go
@@ -0,0 +1,21 @@
+package pointers
+
+import "fmt"
+
+type Tugbacoin int
+
+func (t Tugbacoin) String() string {
+	return fmt.Sprintf("%d TGBC", t)
+}
+
+type Wallet struct {
+	balance Tugbacoin
+}
+
+func (w *Wallet) Deposit(amount Tugbacoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Tugbacoin {
+	return w.balance
+}
