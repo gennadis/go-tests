abdf46fd728d20973c13d81e69dedbc06f8e2670
Gennady Badinov <gennady.badinov@gmail.com>
2024-04-08 12:38:04 +0300

    test: withdraw

--- pointers/walltet_test.go
+++ pointers/walltet_test.go
@@ -3,13 +3,30 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(10)
 
-	got := wallet.Balance()
-	want := Tugbacoin(10)
+		got := wallet.Balance()
+		want := Tugbacoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+
+	})
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(10)
+		wallet.Withdraw(5)
+
+		got := wallet.Balance()
+		want := Tugbacoin(5)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+
+	})
+
 }
