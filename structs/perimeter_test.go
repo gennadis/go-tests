01d6e315fc6052f50caf653bef6c175889c30ff9
Gennady Badinov <gennady.badinov@gmail.com>
2024-04-08 12:01:56 +0300

    test: add triangle tests

--- structs/perimeter_test.go
+++ structs/perimeter_test.go
@@ -5,17 +5,18 @@ import "testing"
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
+		{shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{shape: Trianlgle{Base: 15, LegOne: 8, LegTwo: 17}, hasArea: 60.0},
 	}
 
 	for _, tt := range areaTests {
 		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 		}
 	}
 
@@ -23,16 +24,17 @@ func TestArea(t *testing.T) {
 func TestPerimeter(t *testing.T) {
 
 	perimeterTests := []struct {
-		shape Shape
-		want  float64
+		shape        Shape
+		hasPerimeter float64
 	}{
-		{Rectangle{12, 6}, 36.0},
-		{Circle{10}, 62.83185307179586},
+		{shape: Rectangle{Width: 12, Height: 6}, hasPerimeter: 36.0},
+		{shape: Circle{Radius: 10}, hasPerimeter: 62.83185307179586},
+		{shape: Trianlgle{Base: 15, LegOne: 8, LegTwo: 17}, hasPerimeter: 40.0},
 	}
 	for _, tt := range perimeterTests {
 		got := tt.shape.Perimeter()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
+		if got != tt.hasPerimeter {
+			t.Errorf("got %g want %g", got, tt.hasPerimeter)
 		}
 	}
 
