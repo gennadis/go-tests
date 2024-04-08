87bf1436123f7299d43cbda8c5a3667b0c3bdc98
Gennady Badinov <gennady.badinov@gmail.com>
2024-04-08 13:02:31 +0300

    test: withdraw amount greater than balance

--- pointers/walltet_test.go
+++ pointers/walltet_test.go
@@ -3,7 +3,6 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-
 	assertBalance := func(t testing.TB, wallet Wallet, want Tugbacoin) {
 		t.Helper()
 		got := wallet.Balance()
@@ -11,20 +10,30 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s want %s", got, want)
 		}
 	}
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
 
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(10)
 		assertBalance(t, wallet, Tugbacoin(10))
-
 	})
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(10)
 		wallet.Withdraw(5)
-
 		assertBalance(t, wallet, Tugbacoin(5))
-
+	})
+	t.Run("insufficient funds", func(t *testing.T) {
+		startingBalance := Tugbacoin(10)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(50)
+		assertError(t, err)
+		assertBalance(t, wallet, startingBalance)
 	})
 
 }
